cb6b0009401455b9d44244c4852a9eda9129e67d
Pavol Hejny <me@pavolhejny.com>
2019-05-25 22:38:07 +0200

    Safer cleanupResponse

--- src/utils/cleanupResponse.ts
+++ src/utils/cleanupResponse.ts
@@ -1,7 +1,11 @@
 import { AbstractModel } from './../model/AbstractModel';
 import { isNull, isUndefined } from 'util';
 
-export function cleanupResponse(row: any, cloned = false) {
+export function cleanupResponse<T>(row: T): Partial<T> {
+    return cleanupResponseCore<T>(row, false);
+}
+
+function cleanupResponseCore<T>(row: T, cloned: boolean): Partial<T> {
     if (!cloned) {
         row = JSON.parse(JSON.stringify(row));
     }
@@ -12,7 +16,7 @@ export function cleanupResponse(row: any, cloned = false) {
         } else if (isNull(row[key])) {
         } else if (isUndefined(row[key])) {
         } else if (typeof row[key] === 'object') {
-            row[key] = cleanupResponse(row[key], true);
+            row[key] = cleanupResponseCore(row[key], true);
         }
     }
 
