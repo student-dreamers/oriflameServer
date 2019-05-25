7181ef13ce87896b0a3ba446a1378499c969788b
Pavol Hejny <me@pavolhejny.com>
2019-05-25 22:27:20 +0200

    cleanupResponse Deep

--- src/utils/cleanupResponse.ts
+++ src/utils/cleanupResponse.ts
@@ -1,8 +1,20 @@
 import { AbstractModel } from './../model/AbstractModel';
+import { isNull, isUndefined } from 'util';
 
-export function cleanupResponse(row: any) {
-    const rowCopy = row.toJSON();
+export function cleanupResponse(row: any, cloned = false) {
+    if (!cloned) {
+        row = JSON.parse(JSON.stringify(row));
+    }
 
-    rowCopy.id = undefined;
-    return rowCopy;
+    for (const key of Object.keys(row)) {
+        if (/^(.*\_)?id$/g.test(key)) {
+            row[key] = undefined;
+        } else if (isNull(row[key])) {
+        } else if (isUndefined(row[key])) {
+        } else if (typeof row[key] === 'object') {
+            row[key] = cleanupResponse(row[key], true);
+        }
+    }
+
+    return row;
 }
