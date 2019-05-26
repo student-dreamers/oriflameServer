c1b8db9a99326a5c37b2300f943cca54ebb6cb9e
Pavol Hejny <me@pavolhejny.com>
2019-05-26 03:47:09 +0200

    Mock

--- sql/reset.sql
+++ sql/reset.sql
@@ -15,8 +15,10 @@ INSERT INTO `data_category` (`id`, `uuid`, `name`, `url_icon`) VALUES
 INSERT INTO `data_product` (`id`, `uuid`, `ean`, `category_id`, `name`, `description`, `url_image`, `url_shop`, `price`) VALUES
 (1,	'test',	'1234',	1,	'test',	'',	'https://media-ce-cdn.oriflame.com/-/media/Images/Editorials/2019/C07/Four-Ways-to-Better-Bone-Health/Top.ashx?u=0101010000&w=484&q=90',	'https://cz.oriflame.com/beautyedit/health-beauty/four-ways-to%20better-bone-health',	4.00),
 (2,	'test2',	'4567',	1,	'test2',	'',	'https://media-ce-cdn.oriflame.com/-/media/Images/Editorials/2019/C07/Five-Weight-Bearing-Exercises-for-Strong-Healthy-Bones/Top.ashx?u=0101010000&w=484&q=90',	'https://cz.oriflame.com/beautyedit/health-beauty/five-weight-bearing-exercises-for-strong-healthy-bones',	2.00),
-(3,	'test3',	'00000000',	2,	'test3',	'',	'https://media-ce-cdn.oriflame.com/-/media/Images/Catalog/Products/_global/4/41/415/41566.ashx?u=0101010000&q=90',	'https://cz.oriflame.com/products/product?code=41566',	22.00)
+(3,	'test3',	'00000000',	2,	'test3',	'',	'https://media-ce-cdn.oriflame.com/-/media/Images/Catalog/Products/_global/4/41/415/41566.ashx?u=0101010000&q=90',	'https://cz.oriflame.com/products/product?code=41566',	22.00),
+(4,	'test4',	'11111111',	1,	'test4',	'',	'https://media-ce-cdn.oriflame.com/-/media/Images/Editorials/2019/C07/Five-Weight-Bearing-Exercises-for-Strong-Healthy-Bones/Top.ashx?u=0101010000&w=484&q=90',	'https://cz.oriflame.com/beautyedit/health-beauty/five-weight-bearing-exercises-for-strong-healthy-bones',	2.00)
 ;
+
 
 INSERT INTO `influence` (`id`, `name`, `description`, `score`) VALUES
 (1,	'Ničí to zvířátka',	'helpppp' , 0.3),
